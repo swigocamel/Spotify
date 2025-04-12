de70627b721e5a21cc5a1b67428f59d330e15ae7
swigocamel <swigocamel@gmail.com>
2025-04-12 10:27:40 +0800

    1. run nodemon while build up docker-compose 2. add retry while connecting to db

--- config/db.js
+++ config/db.js
@@ -1,13 +1,24 @@
 // src/config/db.js
 const AppDataSource = require("./data-source");
 
-const connectDB = async () => {
-  try {
-    await AppDataSource.initialize();
-    console.log("✅ 資料庫連線成功！");
-  } catch (err) {
-    console.error("❌ 資料庫連線失敗：", err);
-  }
-};
+const connectDB = async (retry = 5) => {
+    while (retry > 0) {
+      try {
+        await AppDataSource.initialize();
+        console.log("✅ 資料庫連線成功！");
+        break;
+      } catch (err) {
+        console.error("❌ 資料庫連線失敗，重試中...", err.message);
+        retry--;
+        await new Promise((res) => setTimeout(res, 3000));
+      }
+    }
+  
+    if (retry === 0) {
+      console.error("⛔ 無法連線至資料庫，請檢查設定");
+      process.exit(1);
+    }
+  };
+  
 
 module.exports = connectDB;
