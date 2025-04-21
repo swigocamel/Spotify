30219ec97be510f6e4c55fa6ebf9b1cecba581c7
swigocamel <swigocamel@gmail.com>
2025-04-21 20:48:02 +0800

    add api/coaches/auth/login add api/coaches/profile

--- controllers/coachController.js
+++ controllers/coachController.js
@@ -4,6 +4,7 @@ const Coach = require("../entities/Coach");
 const { IsValidString, IsValidEmail, IsValidPassword } = require("../utils/validation");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const coachController = {
     async postSignUp (req, res, next) {
@@ -109,6 +110,95 @@ const coachController = {
             status: true,
             data: {
                 coachList
+            }
+        })
+    },
+
+    async postLogin (req, res, next ) {
+        const { email, password } = req.body;
+
+        if (!IsValidString(email) || !IsValidEmail(email) || !IsValidString(password)) {
+            return res.status(400).json({
+                status: false,
+                message: '欄位未填寫正確',
+            })
+        }
+
+        const coachRepo = AppDataSource.getRepository(Coach);
+        const coach = await coachRepo.findOne({ where: { email } });
+
+        if (!coach) {
+            return res.status(401).json({
+                status: false,
+                message: '帳號或密碼錯誤',
+            })
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, coach.password);
+        if (!isPasswordMatch) {
+            return res.status(401).json({
+                status: false,
+                message: '密碼錯誤',
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                id: coach.id,
+                email: coach.email,
+                nickname: coach.nickname,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '3d' }
+        );
+
+        res.status(200).json({
+            status: true,
+            data: {
+                token,
+                coach: {
+                    id: coach.id,
+                    realname: coach.realname,
+                    nickname: coach.nickname,
+                    email: coach.email
+                }
+            }
+        })
+    },
+
+    async getCoachProfile (req, res, next) {
+        const coachRepo = AppDataSource.getRepository(Coach);
+        const coach = await coachRepo.findOne({ where: { id: req.user.id } });
+
+        if (!coach) {
+            return res.status(400).json({
+                status: false,
+                message: '使用者不存在'
+            })
+        }
+
+        res.status(200).json({
+            status: true,
+            data: {
+                coach: {
+                    id: coach.id,
+                    realname: coach.realname,
+                    email: coach.email,
+                    nickname: coach.nickname,
+                    job_title: coach.job_title,
+                    skill: coach.skill,
+                    skill_description: coach.skill_description,
+                    experience_years: coach.experience_years,
+                    experience: coach.experience,
+                    license_url: coach.license_url,
+                    about_me: coach.about_me,
+                    id_number: coach.id_number,
+                    profile_image_url: coach.profile_image_url,
+                    background_image_url: coach.background_image_url,
+                    bankbook_copy_url: coach.bankbook_copy_url,
+                    phone_number: coach.phone_number,
+                    birthday: coach.birthday,
+                }
             }
         })
     }
--- routes/coaches.js
+++ routes/coaches.js
@@ -1,10 +1,13 @@
 // src/routes/coaches.js
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/coachController');
+const coachController = require('../controllers/coachController');
 const handleErrorAsync = require('../utils/handleErrorAsync');
+const authMiddleware = require('../middleware/authMiddleware');
 
-router.post('/auth/signup', handleErrorAsync(userController.postSignUp));
-router.get('/list', handleErrorAsync(userController.getCoachList));
+router.post('/auth/signup', handleErrorAsync(coachController.postSignUp));
+router.get('/list', handleErrorAsync(coachController.getCoachList));
+router.post('/auth/login', handleErrorAsync(coachController.postLogin));
+router.get('/profile', authMiddleware, handleErrorAsync(coachController.getCoachProfile));
 
 module.exports = router;
