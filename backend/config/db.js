53dea56c56d7b03278921208c0c2887d046949c9
swigocamel <swigocamel@gmail.com>
2025-04-27 14:04:10 +0800

    sync env name

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 module.exports = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   synchronize: process.env.DB_SYNCHRONIZE === 'true',
   ssl: process.env.DB_ENABLE_SSL === 'true' ? { rejectUnauthorized: false } : false
 }
